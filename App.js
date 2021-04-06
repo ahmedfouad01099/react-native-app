43ec1d89ffda797f6fa6ce779713f483ff15b76a
Ahmed Fouad <ahmedfouad01099@gmail.com>
2021-04-06 18:42:12 +0200

    adding routing using javascript logic for rendering components

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 import Header from "./components/Header";
+import GameOverScreen from "./screens/GameOverScreen";
+import GameScreen from "./screens/GameScreen";
 import StartGameScreen from "./screens/StartGame";
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const [guessRounds, setGuessRounds] = useState(0);
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  const gameOverHandler = (numOfRound) => {
+    setGuessRounds(numOfRound);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen />;
+  }
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
