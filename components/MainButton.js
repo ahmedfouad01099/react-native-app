fd9fc3e620e54fdb455ec5aee625ce7265bf6af5
Ahmed Fouad <ahmedfouad01099@gmail.com>
2021-05-30 18:39:48 +0200

    changing some styling depending on different platforms

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,18 +1,35 @@
 import React from "react";
-import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
 import colors from "../constants/colors";
 
 function MainButton(props) {
+  let ButtonComponent = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version > 21) {
+    ButtonComponent = TouchableNativeFeedback;
+  }
   return (
-    <TouchableOpacity activeOpacity={0.5} style={styles.button} onPress={props.onPress}>
-      <View style={styles.buttontext}>
-        <Text>{props.children}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      <ButtonComponent activeOpacity={0.5} onPress={props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttontext}>{props.children}</Text>
+        </View>
+      </ButtonComponent>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 25,
+    overflow: "hidden",
+  },
   button: {
     backgroundColor: colors.primary,
     paddingVertical: 12,
